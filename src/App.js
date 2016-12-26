2ba7d3d76e28996263cb2f2dbe7b32b35a94c453
Kporal <kporal@gmx.fr>
2016-12-26 14:58:17 +0100

    create a message list in the app state and list them in the chat component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import Chat from './Chat';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // set the initial state
+    this.state = {
+      messages: [
+        { time: new Date(2016,10,10).getTime(), value: "Hello"},
+        { time: new Date(2016,10,11).getTime(), value: "Hi"},
+        { time: new Date(2016,10,12).getTime(), value: "Hoz qre you?"},
+      ]
+    };
+  }
+
   render() {
     return (
       <div>
-        <Chat title="Chat A" />
+        <Chat title="Chat A" messages={this.state.messages} />
       </div>
     );
   }
--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
 class Chat extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+
+    const messageList = this.props.messages.map(msg =>
+      <li key={msg.time.toString()}>{msg.value}</li>
+    );
+
     return (
       <div>
         <h1>{this.props.title}</h1>
-        <textarea></textarea>
+        <ul>{messageList}</ul>
         <form>
           <input type="text" name="entry" />
           <button>Ok</button>
