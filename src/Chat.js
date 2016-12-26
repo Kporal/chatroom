41cf72640b54d7c0e68ac7253b4b301dcc3d61ea
Kporal <kporal@gmx.fr>
2016-12-26 15:07:06 +0100

    save the user input value as message in the chat

--- src/Chat.js
+++ src/Chat.js
@@ -3,13 +3,21 @@ import React, { Component } from 'react';
 class Chat extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      msg: ''
+    };
     // bind this
     this.pushMessage = this.pushMessage.bind(this);
+    this.handleMsgChange = this.handleMsgChange.bind(this);
   }
 
   pushMessage(e) {
     e.preventDefault();
-    this.props.onMessage({ time: Date.now(), value: "Fine, thank you!" });
+    this.props.onMessage({ time: Date.now(), value: this.state.msg });
+  }
+
+  handleMsgChange(e) {
+    this.setState({ msg: e.target.value });
   }
 
   render() {
@@ -22,7 +30,7 @@ class Chat extends Component {
         <h1>{this.props.title}</h1>
         <ul>{messageList}</ul>
         <form>
-          <input type="text" name="entry" />
+          <input type="text" name="msg" value={this.state.value} onChange={this.handleMsgChange} />
           <button onClick={this.pushMessage}>Ok</button>
         </form>
       </div>
